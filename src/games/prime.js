5b40cdac14c3c1530c3996d8aac3029ae376735d
Arthur <43511417+turchinskki@users.noreply.github.com>
2018-10-27 13:25:23 +0500

    Add files via upload

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,16 +4,16 @@ import getRandom from '../utils';
 const task = 'Answer "Yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (n) => {
-  if (n < 2) return 'no';
+  if (n < 2) return false;
   for (let i = 2; i < n; i += 1) {
-    if (n % i === 0) return 'no';
+    if (n % i === 0) return false;
   }
-  return 'yes';
+  return true;
 };
 
 const getQuestionAnswer = () => {
   const number = getRandom(1, 17);
-  const trueAnswer = isPrime(number);
+  const trueAnswer = isPrime(number) === true ? 'yes' : 'no';
   const question = String(number);
   return { question, trueAnswer };
 };
--- src/games/progression.js
+++ src/games/progression.js
@@ -3,18 +3,19 @@ import getRandom from '../utils';
 
 const task = 'What number is missing in this progression?';
 
+const progressionLenght = 10;
+
 const getQuestionAnswer = () => {
-  const arrayLenght = 10;
-  const arrayIndex = getRandom(0, 9);
-  const array = [];
+  const hiddenElementPosition = getRandom(0, 9);
+  const progression = [];
   const firstNumber = getRandom(1, 3);
-  const stepArray = getRandom(2, 4);
-  for (let i = 0; i < arrayLenght; i += 1) {
-    array[i] = firstNumber + (stepArray * i);
+  const stepProgression = getRandom(2, 4);
+  for (let i = 0; i < progressionLenght; i += 1) {
+    progression[i] = firstNumber + (stepProgression * i);
   }
-  const trueAnswer = String(array[arrayIndex]);
-  array[arrayIndex] = '..';
-  const question = array.join(' ');
+  const trueAnswer = String(progression[hiddenElementPosition]);
+  progression[hiddenElementPosition] = '..';
+  const question = progression.join(' ');
   return { question, trueAnswer };
 };
 
